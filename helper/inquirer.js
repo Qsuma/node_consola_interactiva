09b94df3429ce5777616e2a86ffe8fe7785b9e87
Qsuma <qsuma01@gmail.com>
2025-05-02 11:05:45 -0400

    Primer Commit

---
+++ helper/inquirer.js
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+import colors from "colors";
+const preguntas = [
+  {
+    type: "list",
+    name: "opcion",
+    message: "Que desea hacer?",
+    choices: [
+      {
+        value: 1, 
+        name: `${"1".green}. Crear una tarea`,
+      },
+      {
+        value: 2,
+        name: `${"2".green}. Listar tareas`,
+      },
+      {
+        value: 3,
+        name: `${"3".green}. Listar tareas completadas`,
+      },
+      {
+        value: 4,
+        name: `${"4".green}. Listar tareas pendientes`,
+      },
+      {
+        value: 5,
+        name: `${"5".green}. Completar tarea(s)`,
+      },
+      {
+        value: 6,
+        name: `${"6".green}. Borrar tarea`,
+      },
+      {
+        value: 0,
+        name: `${"0".green}. Salir\n`,
+      },
+    ],
+  },
+];
+
+export const inquirerMenu = async () => {
+  console.clear();
+  console.log("===========================".green);
+  console.log("===Seleccione una Opción===".green);
+  console.log("===========================\n".green);
+
+  const { opcion } = await inquirer.prompt(preguntas);
+  return opcion;
+};
